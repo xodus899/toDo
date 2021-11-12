407471f593dbfe7e5003bc32ac7de4b63b4450dd
Christopher Justice <19810192+xodus899@users.noreply.github.com>
2021-11-12 16:08:06 -0500

    Update date.js

--- public/javascript/date.js
+++ public/javascript/date.js
@@ -1,16 +1,26 @@
-const getDate = () => {
+module.exports.currentDate = () => {
 
   //get date
-  let today = new Date();
+  const today = new Date();
   // set options to show date options dynamically.
-  let options = {
+  const options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
   // set day to return string portion of date
-  let day = today.toLocaleDateString("en-US", options)
-  return day;
+  return today.toLocaleDateString("en-US", options)
 }
 
-module.exports = getDate;
+
+module.exports.dayOfWeek = () => {
+
+  //get date
+  const today = new Date();
+  // set options to show date options dynamically.
+  const options = {
+    weekday: "long",
+  };
+  // set day to return string portion of date
+  return today.toLocaleDateString("en-US", options)
+}
